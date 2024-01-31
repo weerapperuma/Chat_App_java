f20a39114350683f28b16df7de4c69fac064e14d
weerapperuma <weerapperumasahiru@gmail.com>
2024-01-31 19:03:29 +0530

    Dashboard for client

---
+++ src/main/java/lk/ijse/controller/LoginFormController.java
@@ -0,0 +1,18 @@
+package lk.ijse.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+
+public class LoginFormController {
+
+    @FXML
+    private Label lblDash;
+
+    @FXML
+    private Pane paneDash;
+    public void initialize(){
+
+    }
+
+}
---
+++ src/main/java/lk/ijse/controller/UserLogingChekFormController.java
@@ -0,0 +1,22 @@
+package lk.ijse.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class UserLogingChekFormController {
+
+    @FXML
+    private TextField txtNameFx;
+
+    @FXML
+    void chatOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtNameOnAction(ActionEvent event) {
+
+    }
+
+}
